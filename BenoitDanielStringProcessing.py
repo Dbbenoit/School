94fd2feb3b27177b5d2e82f8a54a353f4ef92cef
Dbbenoit <148245375+Dbbenoit@users.noreply.github.com>
2026-04-18 18:24:39 -0400

    home

---
+++ BenoitDanielStringProcessing.py
@@ -0,0 +1,20 @@
+def main():
+
+    userName:str= str(input("Please enter your name: "))
+    print("Welcome, " + str(userName))
+    roundsLeft:int = 10
+    print()
+    displayMenu()
+    while (roundsLeft>0):
+        userChoice:int = int(input(userName + ", what would you like to do? ")
+            while ()
+
+
+def displayMenu():
+    print("1. Test the Yes Function")
+    print("2. Test the isComplexPassword function")
+    print("3. Test the calcWordSimilarity function")
+    print("4. Test teh computGCPercentage function (if done)")
+    print("5. Press 0 to exit")
+
+main()
